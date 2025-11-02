cebad9ef1029bf3be90d4fa85244c5b5a1ec3e57
Gee <alickoamarko@gmail.com>
2025-11-02 09:55:31 +0100

    WIP

--- GProx.py
+++ GProx.py
@@ -2,7 +2,18 @@ import sys
 import ipaddress
 import socket
 import pyaudio
+import pyogg
 import openal
+
+
+MAX_CLIENTS = 8
+MAX_PACKET_SIZE = 65536
+
+
+class PacketType:
+        SET_NAME = 0
+        VOICE_DATA = 1
+        POSITION_DATA = 2
 
 
 if len(sys.argv) < 2:
@@ -18,6 +29,7 @@ except Exception as e:
         print(e)
         exit(1)
 
+ip = None
 if len(sys.argv) == 3:
         try:
                 ipaddress.IPv6Address(sys.argv[2])
@@ -25,7 +37,24 @@ if len(sys.argv) == 3:
                 print(f"ERROR: Provided IPv6 address {sys.argv[2]} is invalid")
                 print(e)
                 exit(1)
-ip = sys.argv[2]
+        ip = sys.argv[2]
 
 
-# TODO: Start server or connect based on `ip`
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+sock.settimeout(0)
+
+if ip:
+        sock.connect((ip, port))
+        while True:
+                pass # TODO: Record, compress, send, receive, decompress, play
+
+else:
+        clients: dict[socket.socket, str] = []
+        sock.bind(("", port))
+        while True:
+                sock.listen(MAX_CLIENTS)
+                clients.append(sock.accept()[0])
+                for i in range(1, len(clients)+1):
+                        pass # TODO
+
+sock.close()
