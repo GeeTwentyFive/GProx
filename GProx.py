94acf0472919185000264a3eafa260329c4f8a69
Gee <alickoamarko@gmail.com>
2025-11-02 09:14:18 +0100

    WIP

--- GProx.py
+++ GProx.py
@@ -1,5 +1,31 @@
-import enet
+import sys
+import ipaddress
+import socket
 import pyaudio
 import openal
 
-# TODO
+
+if len(sys.argv) < 2:
+        print("USAGE: GProx <PORT> [SERVER_IPv6_ADDRESS]>")
+        print("(If you want to host the server, enter only PORT)")
+        exit(1)
+
+
+try:
+        port = int(sys.argv[1])
+except Exception as e:
+        print(f"ERROR: Provided port {sys.argv[1]} is invalid")
+        print(e)
+        exit(1)
+
+if len(sys.argv) == 3:
+        try:
+                ipaddress.IPv6Address(sys.argv[2])
+        except Exception as e:
+                print(f"ERROR: Provided IPv6 address {sys.argv[2]} is invalid")
+                print(e)
+                exit(1)
+ip = sys.argv[2]
+
+
+# TODO: Start server or connect based on `ip`
