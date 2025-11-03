12108602a77cc344fc980864bc8afdc296ff585c
Gee <alickoamarko@gmail.com>
2025-11-03 12:50:52 +0100

    WIP

--- GProx_Client.cpp
+++ GProx_Client.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string>
 #include <thread>
+#include <mutex>
 
 #define ENET_IMPLEMENTATION
 #include "libs/enet.h"
@@ -35,16 +36,34 @@ typedef enum {
 } PacketType;
 
 
+ENetPeer* gprox_server;
+std::mutex gprox_server_mutex;
 std::vector<PeerData> remote_peer_data;
 
 int bRecord = 0;
 
 
 void AudioInputData_Callback(const void* pData, ma_uint32 frameCount) {
-        // TODO: Send as audio data packet
+        if (bRecord) {
+                unsigned char* packet_data = (unsigned char*)malloc(1 + sizeof(float)*frameCount);
+                packet_data[0] = PACKET_TYPE_AUDIO_DATA;
+                memcpy(&packet_data[1], pData, sizeof(float));
+                gprox_server_mutex.lock();
+                enet_peer_send(
+                        gprox_server,
+                        0,
+                        enet_packet_create(
+                                packet_data,
+                                1 + sizeof(float)*frameCount,
+                                ENET_PACKET_FLAG_UNSEQUENCED
+                        )
+                );
+                gprox_server_mutex.unlock();
+                free(packet_data);
+        }
 }
 
-void IPC_Server(enet_uint16 port, ENetPeer* gprox_server) {
+void IPC_Server(enet_uint16 port) {
         ENetAddress address;
         address.host = ENET_HOST_ANY;
         address.port = port;
@@ -66,22 +85,29 @@ void IPC_Server(enet_uint16 port, ENetPeer* gprox_server) {
         while (true) {
                 while (enet_host_service(server, &event, 1) > 0) {
                         if (event.type == ENET_EVENT_TYPE_RECEIVE) {
-                                if (event.packet->dataLength != sizeof(PeerData)+1) {
+                                if (event.packet->dataLength != sizeof(PeerData)) {
                                         std::cout << "IPC ERROR: Input data size " << event.packet->dataLength << " does not match target " << sizeof(PeerData) << std::endl;
                                 }
-                                enet_packet_resize(event.packet, sizeof(PeerData)+1);
+
+                                unsigned char* local_peer_data_packet_data = (unsigned char*)malloc(1 + sizeof(PeerData));
+                                local_peer_data_packet_data[0] = PACKET_TYPE_PEER_DATA;
                                 memcpy(
-                                        &event.packet->data[1],
+                                        &local_peer_data_packet_data[1],
                                         event.packet->data,
                                         sizeof(PeerData)
                                 );
-                                event.packet->data[0] = PACKET_TYPE_PEER_DATA;
-
+                                gprox_server_mutex.lock();
                                 enet_peer_send(
                                         gprox_server,
                                         0,
-                                        event.packet
+                                        enet_packet_create(
+                                                local_peer_data_packet_data,
+                                                1 + sizeof(PeerData),
+                                                event.packet->flags
+                                        )
                                 );
+                                gprox_server_mutex.unlock();
+                                free(local_peer_data_packet_data);
                         }
                 }
         }
@@ -154,10 +180,6 @@ int main(int argc, char* argv[]) {
         atexit(enet_deinitialize);
 
 
-        // Start IPC (Inter-Process Communication) Server
-        std::thread(IPC_Server, port+1).detach();
-
-
         ENetHost* client = enet_host_create(NULL, 1, 1, 0, 0);
         if (client == NULL) {
                 std::cout << "ERROR: Failed to create ENet client" << std::endl;
@@ -167,8 +189,8 @@ int main(int argc, char* argv[]) {
         ENetAddress address;
         enet_address_set_host_new(&address, argv[2]);
         address.port = port;
-        ENetPeer* server = enet_host_connect(client, &address, 1, 0);
-        if (server == NULL) {
+        gprox_server = enet_host_connect(client, &address, 1, 0);
+        if (gprox_server == NULL) {
                 std::cout << "ERROR: enet_host_connect() failed" << std::endl;
                 return 1;
         }
@@ -180,13 +202,16 @@ int main(int argc, char* argv[]) {
         ) {
                 std::cout << "Connected" << std::endl;
         } else {
-                enet_peer_reset(server);
+                enet_peer_reset(gprox_server);
                 std::cout << "ERROR: Failed to connect to server" << std::endl;
                 return 1;
         }
 
         // Start client listener after connecting
         std::thread(Listen, client).detach();
+
+        // Start IPC (Inter-Process Communication) Server
+        std::thread(IPC_Server, port+1).detach();
 
 
         // Bind V to push-to-talk
@@ -195,7 +220,7 @@ int main(int argc, char* argv[]) {
                 return 1;
         }
 
-        if (AudioInputMonitor__Monitor(ma_format_unknown, 1, 48000, AudioInputData_Callback) != 0) {
+        if (AudioInputMonitor__Monitor(ma_format_f32, 1, 48000, AudioInputData_Callback) != 0) {
                 std::cout << "ERROR: Failed to start monitoring audio input" << std::endl;
         }
 
