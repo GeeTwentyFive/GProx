88fa00a38de322e031840654f500e35fcf1451b7
Gee <alickoamarko@gmail.com>
2025-11-04 01:50:05 +0100

    miniaudio build fix

--- libs/AudioInputMonitor.h
+++ libs/AudioInputMonitor.h
@@ -5,15 +5,15 @@
 
 #include <stdio.h>
 
-#define MINIAUDIO_IMPLEMENTATION
-#define MA_NO_DECODING
-#define NA_NO_ENCODING
-#define MA_NO_WAV
-#define MA_NO_FLAC
-#define MA_NO_MP3
-#define MA_NO_RESOURCE_MANAGER
-#define MA_NO_NODE_GRAPH
-#define MA_NO_GENERATION
+// #define MINIAUDIO_IMPLEMENTATION
+// #define MA_NO_DECODING
+// #define NA_NO_ENCODING
+// #define MA_NO_WAV
+// #define MA_NO_FLAC
+// #define MA_NO_MP3
+// #define MA_NO_RESOURCE_MANAGER
+// #define MA_NO_NODE_GRAPH
+// #define MA_NO_GENERATION
 #include "miniaudio.h"
 
 
