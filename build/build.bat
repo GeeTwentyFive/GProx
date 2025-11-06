8eb877d4ed9909ee32568fbc63d1aafe4c0e8ace
Gee <alickoamarko@gmail.com>
2025-11-06 14:46:09 +0100

    Updated build.bat for Windows

--- build/build.bat
+++ build/build.bat
@@ -1,2 +1,2 @@
-clang++ -O3 ../GProx_Server.cpp -o GProx_Server.exe
-clang++ -O3 ../GProx_Client.cpp ../libs/libuiohook/libuiohook_windows.a -I../libs ../libs/SFML/libsfml-audio-s_windows.a ../libs/SFML/libsfml-system-s_windows.a -o GProx_Client.exe
+g++ -O3 ../GProx_Server.cpp -lws2_32 -lwinmm -o GProx_Server.exe
+g++ -O3 ../GProx_Client.cpp -lws2_32 -lwinmm ../libs/libuiohook/libuiohook_windows.a -I../libs ../libs/SFML/libsfml-audio-s_windows.a ../libs/SFML/libsfml-system-s_windows.a -o GProx_Client.exe
